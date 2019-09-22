c3915ca15328d2c2e42bd652c6eb5077c936cb8d
serega <11serega22@gmail.com>
2019-09-22 09:03:49 +0300

    it adds main file

--- cmd/js/main.go
+++ cmd/js/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
 
+//GOARCH=wasm GOOS=js go build -o lib.wasm main.go
+//go test -cpuprofile profile.out
+//go tool pprof --web profile.out
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	select{}
+}
 
