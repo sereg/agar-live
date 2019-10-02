3eed7acc8165fb589465eb5594748dd0cc616e54
sergei dovydenko <dovydenko@mail.com>
2019-10-02 11:09:15 +0300

    it separates logic chousing closest animal and plant

--- object/alive/animal/behavior/ai-v1.go
+++ object/alive/animal/behavior/ai-v1.go
@@ -25,42 +25,58 @@ func (a *aiV1) SetDirection(self animal.Animal, animals []alive.Alive, plants []
 	if len(dangerous.obj) > 0 {
 		sum := vector.GetVectorByPoint(self.GetX(), self.GetY(), self.GetX(), self.GetY())
 		for _, v := range dangerous.obj {
-			sum = vector.Add(sum, v)
+			sum = vector.Add(sum, v.vec)
 		}
 		a.direction.SetCrd(sum.GetPointFromVector(self.GetX(), self.GetY()))
 		a.setCrdByDirection(self, oldDirection)
-		//println("dangerous")
 		return
 	}
 	var closest alive.Alive
 	closestFn := func() alive.Alive {
-		closest = getClosest(self, append(animals, plants...))
+		closest = closestFn(self, animals, plants)
 		return closest
 	}
 	if (len(animals) == 0 && len(plants) == 0) || closestFn() == nil {
 		a.simple.SetDirection(self, nil, nil)
-		//println("simple", len(animals), len(plants), closest)
 		return
 	}
 	a.direction.SetCrd(closest.GetX(), closest.GetY())
 	a.setCrdByDirection(self, oldDirection)
-	//println("pursue")
+}
+
+func closestFn(self animal.Animal, animals []alive.Alive, plants []alive.Alive) alive.Alive {
+	closestFnAn := func() alive.Alive {
+		return getClosest(self, animals)
+	}
+	closestFnPl := func() alive.Alive {
+		return getClosest(self, plants)
+	}
+	if closest := closestFnAn(); closest == nil {
+		return closestFnPl()
+	} else {
+		return closest
+	}
+}
+
+type dp struct{
+	vec vector.Vector
+	name string
 }
 
 type dangerObj struct {
-	obj []vector.Vector
+	obj []dp
 }
 
-func (d *dangerObj) add(x1, y1, x2, y2, vision float64) {
+func (d *dangerObj) add(x1, y1, x2, y2, vision float64, name string) {
 	x2, y2 = getXYWithLength(x1, y1, x2, y2, vision)
 	x2, y2 = x1+x2, y1+y2
 	vec := vector.GetVectorByPoint(x2, y2, x1, y1)
 	for _, v := range d.obj {
-		if vector.Compare(v, vec) {
+		if vector.Compare(v.vec, vec) {
 			return
 		}
 	}
-	d.obj = append(d.obj, vec)
+	d.obj = append(d.obj, dp{vec: vec, name: name})
 }
 
 func dangerous(el animal.Animal, animals []alive.Alive) dangerObj {
@@ -68,7 +84,7 @@ func dangerous(el animal.Animal, animals []alive.Alive) dangerObj {
 	for i := 0; i < len(animals); i++ {
 		el1 := animals[i]
 		if el != nil && el1 != nil && !el1.GetDead() && el1.GetSize()/el.GetSize() > _const.EatRatio {
-			danObj.add(el.GetX(), el.GetY(), el1.GetX(), el1.GetY(), el.GetVision())
+			danObj.add(el.GetX(), el.GetY(), el1.GetX(), el1.GetY(), el.GetVision(), el1.GetName())
 		}
 	}
 	return danObj
@@ -80,9 +96,6 @@ func getClosest(el animal.Animal, animals []alive.Alive) alive.Alive {
 	mass := 0.0
 	for i := 0; i < len(animals); i++ {
 		el1 := animals[i]
-		if el1.GetSize() < mass {
-			break
-		}
 		var distRes float64
 		distFn := func() float64 {
 			distRes = geom.GetDistanceByCrd(el.GetCrd(), el1.GetCrd())
