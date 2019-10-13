d7cae826bd98ba61509df1882c8d448618082250
serega <11serega22@gmail.com>
2019-10-13 11:10:14 +0300

    refactoring ai separetes all activities in strategy

--- object/alive/animal/behavior/ai-v1.go
+++ object/alive/animal/behavior/ai-v1.go
@@ -69,54 +69,208 @@ func tD(speed, distance float64, cycle uint64) uint64{
 	return uint64(distance/speed*1.1) + cycle
 }
 
+type strategy struct{
+	priority  uint8
+	mem bool
+	condition func() bool
+	reason func() string
+	action func() object.Crd
+}
+
 func (a *aiV1) Direction(self animal.Animal, animals []alive.Alive, plants []alive.Alive, cycle uint64) object.Crd {
 	dangerous := dangerous(self, animals)
-	//TODO bypass obstacles
-	if len(dangerous.obj) > 0 { //TODO hide in poison plant if size of them more then object
-		reason := dangerous.Names()
-		if valid, crd := a.mem.checkByReason(running, cycle, reason); valid {
-			a.direction.SetCrd(crd.GetX(), crd.GetY())
-			return a.direction
-		}
-		sum := vector.GetVectorByPoint(self.GetX(), self.GetY(), self.GetX(), self.GetY())
-		for _, v := range dangerous.obj {
-			sum = vector.Add(sum, v.vec)
-		}
-		sum = checkEdge2(sum, self.GetX(), self.GetY(), a.w, a.h, self.Vision())
-		x, y := sum.GetPointFromVector(self.GetX(), self.GetY())
-		a.mem.set(running, tD(self.Speed(), self.Vision(), cycle), reason, object.NewCrd(x, y))
-		a.direction.SetCrd(x, y)
-		return a.direction
-	}
-	if valid, crd := a.mem.check(eating, cycle); valid {
-		a.direction.SetCrd(crd.GetX(), crd.GetY())
-		return a.direction
-	}
+	plants, poisons := poisons(self, plants)
 	var closest alive.Alive
 	closestFn := func() alive.Alive {
+		if len(animals) == 0 && len(plants) == 0 {
+			return nil
+		}
 		closest = closestFn(self, animals, plants)
 		return closest
 	}
-	reason := strconv.Itoa(len(animals))+"-"+strconv.Itoa(len(plants))
-	if valid, crd := a.mem.checkByReason(eating, cycle, reason); valid {
-		//TODO send split signal if size more than ration and target is alive
-		a.direction.SetCrd(crd.GetX(), crd.GetY())
-		return a.direction
+	//TODO bypass obstacles
+	strategies := []strategy{
+		strategy{//running
+			priority: running,
+			mem: true,
+			condition: func() bool {
+				return len(dangerous.obj) > 0
+			},
+			reason: func() string {
+				return strconv.Itoa(len(dangerous.obj))+"-"+strconv.Itoa(len(poisons))
+			},
+			action: func() object.Crd {
+				sum := vector.GetVectorByPoint(self.GetX(), self.GetY(), self.GetX(), self.GetY())
+				for _, v := range dangerous.obj {
+					sum = vector.Add(sum, v.vec)
+				}
+				sum = checkEdge2(sum, self.GetX(), self.GetY(), a.w, a.h, self.Vision())
+				x, y := sum.GetPointFromVector(self.GetX(), self.GetY())
+				//TODO hide in poison plant if size of them more then object
+				return object.NewCrd(x, y)
+			},
+		},
+		strategy{//running from memory
+			priority: running,
+			mem: false,
+			condition: func() bool {
+				valid, _ := a.mem.check(running, cycle)
+				return valid
+			},
+			action: func() object.Crd {
+				_, crd := a.mem.check(running, cycle)
+				return crd
+			},
+		},
+		strategy{//eating
+			priority: eating,
+			mem: true,
+			condition: func() bool {
+				return closestFn() != nil
+			},
+			reason: func() string {
+				return strconv.Itoa(len(animals))+"-"+strconv.Itoa(len(dangerous.obj))+"-"+strconv.Itoa(len(poisons))
+			},
+			action: func() object.Crd {
+				//TODO send split signal if size more than ration and target is alive
+				return object.NewCrd(closest.GetX(), closest.GetY())
+			},
+		},
+		strategy{//default
+			mem: false,
+			condition: func() bool {
+				return true
+			},
+			action: func() object.Crd {
+				return a.simple.Direction(self, nil, nil, 0)
+			},
+		},
 	}
-	if (len(animals) == 0 && len(plants) == 0) || closestFn() == nil {
-		return a.simple.Direction(self, nil, nil, 0)
+	for _, strategy := range strategies {
+		if strategy.condition() {
+			reason := ""
+			if strategy.mem {
+				reason = strategy.reason()
+				if valid, crd := a.mem.checkByReason(strategy.priority, cycle, reason); valid {
+					a.direction.SetCrd(crd.GetX(), crd.GetY())
+					break
+				}
+			}
+			crd := strategy.action()
+			if strategy.mem {
+				a.mem.set(running, tD(self.Speed(), self.Vision(), cycle), reason, crd)
+			}
+			a.direction.SetCrd(crd.GetX(), crd.GetY())
+			break
+		}
 	}
-	a.mem.set(eating, tD(self.Speed(), self.Vision(), cycle), reason, object.NewCrd(closest.GetX(), closest.GetY()))
-	a.direction.SetCrd(closest.GetX(), closest.GetY())
 	return a.direction
 }
 
-func nameAlive(al []alive.Alive) string {
-	names := make([]string, len(al))
-	for k, v := range al {
-		names[k] = v.Group()
+func closestFn(self animal.Animal, animals []alive.Alive, plants []alive.Alive) alive.Alive {
+	closestFnAn := func() alive.Alive {
+		return getClosest(self, animals)
+	}
+	closestFnPl := func() alive.Alive {
+		return getClosest(self, plants)
+	}
+	if closest := closestFnAn(); closest == nil {
+		return closestFnPl()
+	} else {
+		return closest
+	}
+}
+
+type dp struct {
+	vec  vector.Vector
+	name string
+}
+
+type dangerObj struct {
+	obj []dp
+}
+
+func (d dangerObj) Names() string {
+	names := make([]string, len(d.obj))
+	for k, v := range d.obj {
+		names[k] = v.name
 	}
 	return strings.Join(names, "")
+}
+
+func (d *dangerObj) add(x1, y1, x2, y2, vision float64, name string) {
+	x2, y2 = getXYWithLength(x1, y1, x2, y2, vision)
+	x2, y2 = x1+x2, y1+y2
+	vec := vector.GetVectorByPoint(x2, y2, x1, y1)
+	for _, v := range d.obj {
+		if vector.Compare(v.vec, vec) {
+			return
+		}
+	}
+	d.obj = append(d.obj, dp{vec: vec, name: name})
+}
+
+func getXYWithLength(x1, y1, x2, y2, dist float64) (x float64, y float64) {
+	vec := vector.GetVectorByPoint(x1, y1, x2, y2)
+	length := vec.Len()
+	ratio := dist / length
+	vec.MultiplyByScalar(ratio)
+	x, y = vec.GetPointFromVector(x2, y2)
+	x, y = x-x2, y-y2
+	return
+}
+
+func dangerous(el animal.Animal, animals []alive.Alive) dangerObj {
+	danObj := dangerObj{}
+	for i := 0; i < len(animals); i++ {
+		el1 := animals[i]
+		if el != nil && el1 != nil && !el1.GetDead() && el1.Size()/el.Size() > _const.EatRatio {
+			danObj.add(el.GetX(), el.GetY(), el1.GetX(), el1.GetY(), el.Vision(), el1.Group())
+		}
+	}
+	return danObj
+}
+
+func poisons(el animal.Animal, plants []alive.Alive) (food []alive.Alive, poisons []alive.Alive) {
+	if el.Size() - _const.MinSizeAlive < _const.MinSizeAlive || el.Count() >= _const.SplitMaxCount {
+		return plants, poisons
+	}
+	food = make([]alive.Alive, 0, len(plants))
+	poisons = make([]alive.Alive, 0, len(plants))
+	for i := 0; i < len(plants); i++ {
+		el1 := plants[i]
+		if el == nil || el1 == nil || el1.GetDead() {
+			continue
+		}
+		if el1.Size() > el.Size() {
+			poisons = append(poisons, el1)
+		} else {
+			food = append(food, el1)
+		}
+	}
+	return
+}
+
+func getClosest(el animal.Animal, els []alive.Alive) alive.Alive {
+	var elRes alive.Alive
+	dist := 9e+5
+	mass := 0.0
+	for i := 0; i < len(els); i++ {
+		el1 := els[i]
+		var distRes float64
+		distFn := func() float64 {
+			distRes = geom.GetDistanceByCrd(el.GetCrd(), el1.GetCrd()) - el.Size()
+			return distRes
+		}
+		if el != nil && el1 != nil && !el1.GetDead() && !el1.Danger() &&
+			el.Size()/el1.Size() > _const.EatRatio &&
+			mass <= el1.Size() && distFn() < dist && distRes < el.Vision() {
+			elRes = el1
+			dist = distRes
+			mass = el1.Size()
+		}
+	}
+	return elRes
 }
 
 func checkEdge2(sum vector.Vector, xobj, yobj, w, h, vision float64) vector.Vector {
@@ -216,90 +370,4 @@ func getSide(len, lenb, angle float64) float64 {
 	lenC2 := len * len
 	lenC3 := 2 * len * lenb * math.Cos(angleC)
 	return math.Sqrt(lenC1 + lenC2 - lenC3)
-}
-
-func closestFn(self animal.Animal, animals []alive.Alive, plants []alive.Alive) alive.Alive {
-	closestFnAn := func() alive.Alive {
-		return getClosest(self, animals)
-	}
-	closestFnPl := func() alive.Alive {
-		return getClosest(self, plants)
-	}
-	if closest := closestFnAn(); closest == nil {
-		return closestFnPl()
-	} else {
-		return closest
-	}
-}
-
-type dp struct {
-	vec  vector.Vector
-	name string
-}
-
-type dangerObj struct {
-	obj []dp
-}
-
-func (d dangerObj) Names() string {
-	names := make([]string, len(d.obj))
-	for k, v := range d.obj {
-		names[k] = v.name
-	}
-	return strings.Join(names, "")
-}
-
-func (d *dangerObj) add(x1, y1, x2, y2, vision float64, name string) {
-	x2, y2 = getXYWithLength(x1, y1, x2, y2, vision)
-	x2, y2 = x1+x2, y1+y2
-	vec := vector.GetVectorByPoint(x2, y2, x1, y1)
-	for _, v := range d.obj {
-		if vector.Compare(v.vec, vec) {
-			return
-		}
-	}
-	d.obj = append(d.obj, dp{vec: vec, name: name})
-}
-
-func getXYWithLength(x1, y1, x2, y2, dist float64) (x float64, y float64) {
-	vec := vector.GetVectorByPoint(x1, y1, x2, y2)
-	length := vec.Len()
-	ratio := dist / length
-	vec.MultiplyByScalar(ratio)
-	x, y = vec.GetPointFromVector(x2, y2)
-	x, y = x-x2, y-y2
-	return
-}
-
-func dangerous(el animal.Animal, animals []alive.Alive) dangerObj {
-	danObj := dangerObj{}
-	for i := 0; i < len(animals); i++ {
-		el1 := animals[i]
-		if el != nil && el1 != nil && !el1.GetDead() && el1.Size()/el.Size() > _const.EatRatio {
-			danObj.add(el.GetX(), el.GetY(), el1.GetX(), el1.GetY(), el.Vision(), el1.Group())
-		}
-	}
-	return danObj
-}
-
-func getClosest(el animal.Animal, els []alive.Alive) alive.Alive {
-	var elRes alive.Alive
-	dist := 9e+5
-	mass := 0.0
-	for i := 0; i < len(els); i++ {
-		el1 := els[i]
-		var distRes float64
-		distFn := func() float64 {
-			distRes = geom.GetDistanceByCrd(el.GetCrd(), el1.GetCrd()) - el.Size()
-			return distRes
-		}
-		if el != nil && el1 != nil && !el1.GetDead() && !el1.Danger() &&
-			el.Size()/el1.Size() > _const.EatRatio &&
-			mass <= el1.Size() && distFn() < dist && distRes < el.Vision() {
-			elRes = el1
-			dist = distRes
-			mass = el1.Size()
-		}
-	}
-	return elRes
 }
