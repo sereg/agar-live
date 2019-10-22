ceea2116b2465311cd69bb1a6d2e25e9a6a43bc0
serega <11serega22@gmail.com>
2019-10-22 22:27:36 +0300

    it adds method checkangle

--- object/alive/animal/behavior/ai/ai-v1.go
+++ object/alive/animal/behavior/ai/ai-v1.go
@@ -24,42 +24,6 @@ const (
 	nothing = 9
 )
 
-type memory struct {
-	valid     bool
-	priority  uint8
-	validTime uint64
-	reason    string
-	crd       crd.Crd
-}
-
-func (m *memory) set(pr uint8, vt uint64, reason string, crd crd.Crd) {
-	m.valid = true
-	m.priority = pr
-	m.validTime = vt
-	m.reason = reason
-	m.crd = crd
-}
-
-func (m *memory) check(pr uint8, cycle uint64) (bool, crd.Crd) {
-	if m.valid && m.validTime < cycle && m.priority >= pr {
-		return true, m.crd
-	}
-	m.reset()
-	return false, m.crd
-}
-
-func (m *memory) checkByReason(pr uint8, cycle uint64, reason string) (bool, crd.Crd) {
-	if m.valid && m.validTime > cycle && m.priority >= pr && m.reason == reason {
-		return true, m.crd
-	}
-	m.reset()
-	return false, m.crd
-}
-
-func (m *memory) reset() {
-	m.valid = false
-}
-
 func NewAiv1(w, h float64) animal.Behavior {
 	return &aiV1{
 		Simple: behavior.NewSimple(w, h),
